28483aaf385df86868cd866d5cb2851297aa4965
MinKyeongTae <kyeongtea92@gmail.com>
2020-06-27 18:58:24 +0900

    Study Lecture; Operator란 무엇이냐~(2)

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -90,6 +90,84 @@ class ViewController: UIViewController {
         }
     }
 
+    func getJson4() -> Observable<String> {
+        // MARK: - Observable의 분기 예시)
+
+        /*
+         /*
+          let ob1: Observable<String> =
+              Observable.just(MEMBER_LIST_URL) // String
+                  .map { URL(string: $0)! } // -> URL
+                  .map { try Data(contentsOf: $0) } // -> Data
+                  .map { JSON($0) } // JSON
+                  .map { $0["name"].stringValue }
+                  .map { $0 + "\n" }
+          */
+
+         // MARK: ob1 == ob2 + ob3
+         // - ob2: 데이터를 다운받아 json 객체로 만든다.
+         let ob2: Observable<JSON> =
+         Observable.just(MEMBER_LIST_URL)
+             .map { URL(string: $0)! }
+             .map { try Data(contentsOf: $0) }
+             .map { JSON($0) }
+             .share()
+
+         // - ob2의 결과 -> ob3으로 분기, name을 전달
+         let ob3 = ob2
+             .flatMap { Observable.from($0.arrayValue) } // JSON
+             .map { $0["name"].stringValue } // JSON
+             .map { $0 + "\n" }
+
+         // - ob2의 결과 -> ob4로 분기, id를 전달
+         let ob4 = ob2
+             .flatMap { Observable.from($0.arrayValue) } // JSON
+             .map { $0["id"].intValue } // JSON
+             .map { "\($0)\n" }
+
+         ob3.subscribe() // ob2 + ob3가 실행된다.
+         ob4.subscribe() // ob2 + ob4가 실행된다.
+         // 위 두줄처럼 하게 되면 JSON 변환을 두번하는 비효율이 발생한다.
+         // 이때 ob2에 share() 연산자를 사용하면 똑같은 작업이 반복되지 않도록 방지해준다.
+         */
+
+        // MARK: - Observable의 병합예시
+
+        /*
+         let ob2: Observable<JSON> =
+         Observable.just(MEMBER_LIST_URL) // String
+             .map { URL(string: $0)! } // URL
+             .map { try Data(contentsOf: $0) } // Data
+             .map { JSON($0) } // JSON
+
+         let ob3 = ob2
+             .flatMap { Observable.from($0.arrayValue) } // JSON
+             .map { $0["name"].stringValue } // JSON
+             .map { $0 + "\n" }
+
+         let ob4 = ob2
+             .flatMap { Observable.from($0.arrayValue) } // JSON
+             .map { $0["id"].intValue } // JSON
+             .map { "\($0)\n" }
+
+         // - merge 연산자 : 두개의 Observable을 합친다. 합칠 두개의 타입이 동일해야한다.
+         // - zip 연산자 : 합칠 두개의 Observable이 와야 동작한다.(그 전까지 기다림) 이후 두개가 오면 합쳐서 작동한다.
+         // Observable.zip(ob3, ob4) { names, ids in names.count + ids.count }.map { i in }
+
+         // - combineLatest 연산자 : 최근 이벤트 발생시마다 각 두개의 Observable을 묶어서 방출(Emision)한다.
+         // Observable.combineLatest(ob3, ob4) { names, ids in names.count + ids.count }
+         */
+
+        return Observable.create { f in
+            guard let url = URL(string: MEMBER_LIST_URL),
+                let data = try? Data(contentsOf: url),
+                let json = String(data: data, encoding: .utf8) else { return Disposables.create() }
+
+            f.onNext(json)
+            return Disposables.create()
+        }
+    }
+
     // MARK: SYNC
 
     var disposable: Disposable?
